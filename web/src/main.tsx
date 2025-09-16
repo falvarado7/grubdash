95947328d6e22a00e35b52da2f5eb2bbf333881a
Francisco Alvarado <francisco.alvarado217@yahoo.com>
2025-09-16 16:54:46 -0500

    feat: added footer

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -14,19 +14,33 @@ import CartPanel from "./components/CartPanel";
 const qc = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={qc}>
-      <CartProvider>
-        <BrowserRouter>
-            <CartPanel />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/orders/new" element={<Home />} />
-          </Routes>
-        </BrowserRouter>
-        <Toaster richColors position="top-right" />
-      </CartProvider>
-    </QueryClientProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <QueryClientProvider client={qc}>
+            <CartProvider>
+                <BrowserRouter>
+                <div className="min-h-screen flex flex-col">
+                    <CartPanel />
+                    <main className="flex-1">
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/dashboard" element={<Dashboard />} />
+                            <Route path="/orders/new" element={<Home />} />
+                        </Routes>
+                    </main>
+                    <footer className="
+                        border-t border-zinc-400 dark:border-zinc-800
+                        bg-zinc-200/60 dark:bg-zinc-900/60
+                        py-6 text-center text-sm text-zinc-500 mt-auto">
+                        <p>GrubDash</p>
+                        <br></br>
+                        <p>Built and designed by Francisco Alvarado</p>
+                        <br></br>
+                        <p>All rights reserved ©</p>
+                    </footer>
+                </div>
+                </BrowserRouter>
+                <Toaster richColors position="top-right" />
+            </CartProvider>
+        </QueryClientProvider>
+    </React.StrictMode>
 );
