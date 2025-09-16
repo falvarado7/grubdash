365cb5f08c58ee302dfe0f738ba9936ee436b4c8
Francisco Alvarado <francisco.alvarado217@yahoo.com>
2025-09-16 16:05:56 -0500

    Add .gitignore and remove tracked ignored files

--- server/GrubDash.Api/obj/Debug/net9.0/GrubDash.Api.AssemblyInfo.cs
+++ server/GrubDash.Api/obj/Debug/net9.0/GrubDash.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrubDash.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6045fcc5ece598968ad6c8045b2f0ecabff1bc2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrubDash.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrubDash.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- server/GrubDash.Domain/obj/Debug/net9.0/GrubDash.Domain.AssemblyInfo.cs
+++ server/GrubDash.Domain/obj/Debug/net9.0/GrubDash.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrubDash.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6045fcc5ece598968ad6c8045b2f0ecabff1bc2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrubDash.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrubDash.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
